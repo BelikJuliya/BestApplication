ddb45efb39919a2c6f23492f40f39864eb20df76
«Yuliya <YBelik@renins.com>
2023-04-18 23:58:51 +0300

    Инициализация рум починена

--- data/src/main/java/com/example/data/db/FilmDataBase.kt
+++ data/src/main/java/com/example/data/db/FilmDataBase.kt
@@ -4,7 +4,7 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.example.data.remote.model.FilmRemoteModel
 
-@Database(entities = [FilmRemoteModel::class], version = 1)
+@Database(entities = [FilmEntity::class], version = 1)
 abstract class FilmDataBase: RoomDatabase() {
     abstract fun filmRemoteDao(): FilmsDao
 }
